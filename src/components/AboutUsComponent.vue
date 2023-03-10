2510ea186a63f70c38d665dfe6dd2ea989b39274
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-03-10 18:54:32 +0100

    update to aboutUsComponent

--- src/components/AboutUsComponent.vue
+++ src/components/AboutUsComponent.vue
@@ -29,6 +29,7 @@
 
   .aboutUsMain{
     display: flex;
+    justify-content: center;
 
     &__header {
       width: 10%;
@@ -36,6 +37,7 @@
       flex-flow: column;
       justify-content: flex-start;
       position: relative;
+
 
       h2{
         transform: rotate(270deg);
@@ -56,13 +58,15 @@
       img {
         width: 100%;
         height: 100%;
+        max-height: 500px;
+        max-width: 400px;
       }
     }
   }
 }
 
 .aboutUsInfos{
-  margin: 0 16px 0 80px;
+  margin: 0 16px 0 88px;
 
   h3 {
     color: $gold600;
@@ -86,6 +90,129 @@
       left: 0;
     }
   }
+}
+
+@media screen and (min-width:600px) and (max-width: 999px) {
+  .aboutUswrapper {
+    padding-top: 45px;
+  
+    .aboutUsMain{
+      display: flex;
+      justify-content: center;
+  
+      &__header {
+        width: 10%;
+        display: flex;
+        flex-flow: column;
+        justify-content: flex-start;
+        position: relative;
+  
+  
+        h2{
+          transform: rotate(270deg);
+          width: fit-content;
+          margin: 0;
+          white-space: nowrap;
+          position: absolute;
+          top: 104px;
+          left: 0px;
+          font-size: $mobileH1;
+          color: $gold600;
+        }
+      }
+  
+      &__image {
+        width: 85%;
+  
+        img {
+          width: 100%;
+          height: 100%;
+          max-height: 500px;
+          max-width: 400px;
+        }
+      }
+    }
+  }
+}
+
+@media screen and (min-width: 1000px) {
+
+  .aboutUswrapper {
+    padding-top: 45px;
+    display: flex;
+  
+    .aboutUsMain{
+        width: 40%;
+        
+      &__header {
+        width: 10%;
+        display: flex;
+        flex-flow: column;
+        justify-content: flex-start;
+        position: relative;
+  
+  
+        h2{
+          transform: rotate(270deg);
+          width: fit-content;
+          margin: 0;
+          white-space: nowrap;
+          position: absolute;
+          top: 150px;
+          left: 0px;
+          font-size: $desktopH3;
+          color: $gold600;
+        }
+      }
+  
+      &__image {
+        width: 100%;
+  
+        img {
+          width: 100%;
+          height: 100%;
+          max-height: 800px;
+          max-width: 800px;
+        }
+      }
+    }
+  }
+  
+  .aboutUsInfos{
+    margin: 0;
+    width: 60%;
+    display: flex;
+    flex-flow: column nowrap;
+    align-content: center;
+    padding-top: 85px;
+  
+    h3 {
+      color: $gold600;
+      line-height: 45px;
+      font-size: $desktopH3;
+      letter-spacing: -1px;
+      padding-bottom: $spc32;
+      width: 85%;
+      font-weight: normal;
+    }
+  
+    p {
+      line-height: 27.6px;
+      font-size: $desktopH6;
+      padding-bottom: 21px;
+      width: 85%;
+    }
+  
+    &__leaf {
+      position: relative;
+  
+      img {
+        position: absolute;
+        left: 0;
+      }
+    }
+  }
+
 }
 
 </style>
