e2f4e1d30522fc3117801140462b62808e0e1423
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-03-22 18:51:54 +0100

    finished shop component

--- src/components/ShopComponent.vue
+++ src/components/ShopComponent.vue
@@ -13,23 +13,35 @@ const windowWidth = window.innerWidth;
         </div>
     </div>
     <div class="shop__categories" v-if="windowWidth >= 1024">
-        <div class="indoorplantsWrapper"> 
-            <div class="indoorplants__image image"></div>
+        <div class="indoorplantsWrapper flex6"> 
+            <div class="indoorplants__image image">
+                <div class="indoorplants--text textstyles">Indoor plants</div>
+            </div>
         </div>
-        <div class="freshflowersWrapper"> 
-            <div class="freshflowers__image image"></div>
+        <div class="freshflowersWrapper flex6"> 
+            <div class="freshflowers__image image">
+                <div class="freshflowers--text textstyles">Fresh flowers</div>
+            </div>
         </div>
-        <div class="flowerboxesWrapper"> 
-            <div class="flowerboxes__image image"></div>
+        <div class="flowerboxesWrapper flex6"> 
+            <div class="flowerboxes__image image">
+                <div class="flowerboxes--text textstyles">Flower boxes</div>
+            </div>
         </div>
-        <div class="wreathsWrapper"> 
-            <div class="wreaths__image image"></div>
+        <div class="wreathsWrapper flex6"> 
+            <div class="wreaths__image image">
+                <div class="wreaths--text textstyles">Wreaths</div>
+            </div>
         </div>
-        <div class="suculentsWrapper"> 
-            <div class="suculents__image image"></div>
+        <div class="suculentsWrapper flex6"> 
+            <div class="suculents__image image">
+                <div class="suculents--text textstyles">Suculents</div>
+            </div>
         </div>
-        <div class="buqietsWrapper"> 
-            <div class="buqiets__image image"></div>
+        <div class="buqietsWrapper flex6"> 
+            <div class="buqiets__image image">
+                <div class="buqiets--text textstyles">Bouqets</div>
+            </div>
         </div>
     </div>
 </template>
@@ -97,81 +109,179 @@ const windowWidth = window.innerWidth;
 }
 
 .shop__categories {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-    grid-column-gap: 40px;
-    grid-row-gap: 40px;
+    display: flex;
+    justify-content: center;
+    gap: 40px;
+    flex-flow: row wrap;
     width: 90%;
     margin: 0 auto;
+    max-width: 1400px;
 
     .image {
         background-size: cover;
         background-repeat: no-repeat;
         height: 100%;
         width: 100%;
+        opacity: 0.8;
     }
 
-    .indoorplantsWrapper { 
-        grid-area: 1 / 1 / 2 / 2; 
-        height: 100%;
+    .flex6 {
+        display: flex;
+        align-items: flex-end;
+        justify-content: flex-start;
+        position: relative;
+    }
+
+    .textstyles {
+        color: $gold600;
+        font-size: 32px;
+        line-height: 38.4px;
+        text-align: center;
+        background-color: $white100;
+        position: absolute;
+        width: fit-content;
         width: 100%;
-        border: 1px solid green;
+        
+    }
+
+    .indoorplantsWrapper {
+            
 
             .indoorplants__image {
                 background-image: url('../assets/indoor__plants.png');
                 border-radius: 225px 225px 0 0;
+                height: 30vw;
+                width: 22.569vw;
+
+                .indoorplants--text {
+                    top: 60%;
+                }
+
             }
         
     }
     .freshflowersWrapper {
-         grid-area: 1 / 2 / 2 / 3;
-         height: 100%;
-         width: 100%;
-         border: 1px solid green;
+
+
+        align-items: flex-start;
             .freshflowers__image {
                 background-image: url('../assets/fresh__flowers.png');
+                height: 31.25vw;
+                width: 22.569vw;
+
+                .freshflowers--text {
+                    top: 45%;
+                }
             }
 
          }
     .flowerboxesWrapper {
-         grid-area: 1 / 3 / 2 / 4;
-         height: 31.25vw;
-         width: 100%;
-         border: 1px solid green;
+        position: relative;
+
+        &:before {
+            content: '';
+            position: absolute;
+            z-index: 1;
+            transform: translateY(-35%);
+            top: 65%;
+            left: 10%;
+            width: 100%;
+            height: 160%;
+            border-right: 2px solid $gold600;
+        }
+
+        &:after{
+            content: '';
+            position: absolute;
+            z-index: 1;
+            transform: translateY(-50%);
+            top: 15%;
+            left: 0%;
+            width: 125%;
+            height: 10%;
+            border-bottom: 2px solid $gold600;
+      
+          }
+
             .flowerboxes__image{
                 background-image: url('../assets/flower__boxes.png');
+                height: 22.569vw;
+                width: 22.569vw;
+
+                .flowerboxes--text {
+                    top: 60%
+                }
             }
          
          }
     .wreathsWrapper {
-         grid-area: 2 / 1 / 3 / 2;
-         height: 31.25vw;
-         width: 100%;
-         border: 1px solid green;
+        position: relative;
+
+        &:before {
+            content: '';
+            position: absolute;
+            z-index: 1;
+            transform: translateY(-35%);
+            top: -15%;
+            left: -10%;
+            width: 100%;
+            height: 165%;
+            border-left: 2px solid $gold600;
+        }
+
+        &:after{
+            content: '';
+            position: absolute;
+            z-index: 1;
+            transform: translateY(-50%);
+            top: 30%;
+            left: -25%;
+            width: 125%;
+            height: 100%;
+            border-bottom: 2px solid $gold600;
+      
+          }
+
+        align-items: flex-start;
             .wreaths__image{
                 background-image: url('../assets/wreaths.png');
+                height: 22.569vw;
+                width: 22.569vw;
+
+                .wreaths--text {
+                    top: 32%;
+                }
             }
 
          }
     .suculentsWrapper {
-         grid-area: 2 / 2 / 3 / 3;
-         height: 31.25vw;
-         width: 100%;
-         border: 1px solid green;
+
+
+        align-items: flex-start;
             .suculents__image{
                 background-image: url('../assets/suculents.png');
+                height: 31.25vw;
+                width: 22.569vw;
+
+                .suculents--text {
+                    top: 48%;
+                }
             }
          
          }
     .buqietsWrapper {
-         grid-area: 2 / 3 / 3 / 4;
-         height: 100%;
-         width: 100%;
-         border: 1px solid green;
+
+
+        align-items: flex-start;
             .buqiets__image{
                 background-image: url('../assets/buqiets.png');
                 border-radius: 0 0 225px 225px;
+                height: 30vw;
+                width: 22.569vw;
+
+                .buqiets--text {
+                    top: 32%;
+                }
             }
 
          }
